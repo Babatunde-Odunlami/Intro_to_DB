b1e639e6418e46c821c8466f9e0e4c68e4400e45
Babatunde-Odunlami <Odunlamibabatunde@outlook.com>
2024-10-18 15:43:26 -0700

    corecting DB task 2

--- task_2.sql
+++ task_2.sql
@@ -15,24 +15,7 @@ mycursor = mydb.cursor()
 mycursor.execute("USE alx_book_store")
 
 try:
-    # Create a table named `Books` (if it doesn't exist)
-    mycursor.execute(
-        """
-        CREATE TABLE IF NOT EXISTS Books (
-        book_id INT PRIMARY KEY NOT NULL,
-        title VARCHAR(130),
-        author_id INT NOT NULL,
-        price DOUBLE,
-        publication_date DATE
-        )
-        """
-    )
-except sql.Error as e:
-    print(f"Error creating Books table")
-
-
-try:
-    # Create a table named `Authors` (if it doesn't exist)
+    # Create a table named `customers` (if it doesn't exist)
     mycursor.execute(
         """
         CREATE TABLE IF NOT EXISTS Authors (
@@ -41,8 +24,25 @@ try:
         )
         """
     )
-except sql.Error as e:
+except mysql.connector.Error as e:
     print(f"Error creating Authors table")
+
+try:
+    # Create a table named `customers` (if it doesn't exist)
+    mycursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS Books (
+        book_id INT PRIMARY KEY NOT NULL,
+        title VARCHAR(130),
+        author_id INT NOT NULL,
+        price DOUBLE,
+        publication_date DATE,
+        FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+        )
+        """
+    )
+except mysql.connector.Error as e:
+    print(f"Error creating Books table")
 
 try:
     # Create a table named `customers` (if it doesn't exist)
@@ -55,81 +55,38 @@ try:
         address TEXT )
         """
     )
-except sql.Error as e:
+except mysql.connector.Error as e:
     print(f"Error creating Customers table")
 
 try:
-    # Create a table named `Orders` (if it doesn't exist)
+    # Create a table named `customers` (if it doesn't exist)
     mycursor.execute(
         """
         CREATE TABLE IF NOT EXISTS Orders (
         order_id INT PRIMARY KEY,
         customer_id INT NOT NULL,
-        order_date DATE )
+        order_date DATE,
+        FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+        )
         """
     )
-except sql.Error as e:
+except mysql.connector.Error as e:
     print(f"Error creating Orders table")
 
 try:
-    # Create a table named `Order_details` (if it doesn't exist)
+    # Create a table named `customers` (if it doesn't exist)
     mycursor.execute(
         """
         CREATE TABLE IF NOT EXISTS Order_Details (
         orderdetailid INT PRIMARY KEY NOT NULL,
         order_id INT,
         book_id INT,
-        quantity DOUBLE)
+        quantity DOUBLE,
+        FOREIGN KEY (order_id) REFERENCES Orders (order_id),
+        FOREIGN KEY (book_id) REFERENCES Books (book_id)
+        )
         """
     )
-except sql.Error as e:
+except mysql.connector.Error as e:
     print(f"Error creating order_details table")
 
-try:
-    mycursor.execute(
-        """ 
-        ALTER TABLE Books
-        ADD CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES Authors (author_id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-        """
-    )
-    print(f"Altering Books table successfull!")
-except sql.Error as e:
-    print(f"Error altering Books table")
-
-try:
-    mycursor.execute(
-        """ 
-        ALTER TABLE Orders
-        ADD CONSTRAINT fk_order FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-        """
-    )
-except sql.Error as e:
-    print(f"Error altering Orders table")
-
-try:
-    mycursor.execute(
-        """ 
-        ALTER TABLE Order_Details
-        ADD CONSTRAINT fk_order_details FOREIGN KEY (order_id) REFERENCES Orders (order_id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-        """
-    )
-except sql.Error as e:
-    print(f"Error altering Orders table")
-
-try:
-    mycursor.execute(
-        """ 
-        ALTER TABLE Order_Details
-        ADD CONSTRAINT fk_book_id FOREIGN KEY (book_id) REFERENCES Books (book_id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-        """
-    )
-except sql.Error as e:
-    print(f"Error altering Orders table")
