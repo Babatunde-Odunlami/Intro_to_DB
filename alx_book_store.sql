cab474e725929c81e94b1ce2f2cc9f42d8904fc8
Babatunde-Odunlami <Odunlamibabatunde@outlook.com>
2024-10-18 15:22:50 -0700

    correcting DB task 0

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -6,19 +6,20 @@ COLLATE utf8mb4_unicode_ci;
 -- specify thr database to use
 USE alx_book_store;
 
+--creating Authors table
+CREATE TABLE IF NOT EXISTS Authors (
+author_id INT PRIMARY KEY NOT NULL,
+author_name VARCHAR(215)
+);
+
 -- create the Books table
 CREATE TABLE IF NOT EXISTS Books (
 book_id INT PRIMARY KEY NOT NULL,
 title VARCHAR(130),
 author_id INT NOT NULL,
 price DOUBLE,
-publication_date DATE
-);
-
---creating Authors table        
-CREATE TABLE IF NOT EXISTS Authors (
-author_id INT PRIMARY KEY NOT NULL,
-author_name VARCHAR(215)
+publication_date DATE,
+FOREIGN KEY (author_id) REFERENCES Authors (author_id)
 );
 
 --creating customers table
@@ -26,13 +27,16 @@ CREATE TABLE IF NOT EXISTS Customers (
 customer_id INT NOT NULL PRIMARY KEY,
 customer_name VARCHAR(215),
 email VARCHAR(215),
-address TEXT);
+address TEXT
+);
 
 --creating Orders table
 CREATE TABLE IF NOT EXISTS Orders (
 order_id INT PRIMARY KEY,
 customer_id INT NOT NULL,
-order_date DATE);
+order_date DATE
+FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
+);
 
 
 --creating Order_Details table
@@ -40,28 +44,8 @@ CREATE TABLE IF NOT EXISTS Order_Details (
 orderdetailid INT PRIMARY KEY NOT NULL,
 order_id INT,
 book_id INT,
-quantity DOUBLE);
+quantity DOUBLE
+FOREIGN KEY (order_id) REFERENCES Orders (order_id)
+FOREIGN KEY (book_id) REFERENCES Books (book_id)
+);
 
--- altering table books
-ALTER TABLE Books
-ADD CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES Authors (author_id)
-ON DELETE CASCADE
-ON UPDATE CASCADE;
-
---altering Order table
-ALTER TABLE Orders
-ADD CONSTRAINT fk_order FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
-ON DELETE CASCADE
-ON UPDATE CASCADE;
-
---altering Order_details table
-ALTER TABLE Order_Details
-ADD CONSTRAINT fk_order_details FOREIGN KEY (order_id) REFERENCES Orders (order_id)
-ON DELETE CASCADE
-ON UPDATE CASCADE;
-
---altering the order_details table
-ALTER TABLE Order_Details
-ADD CONSTRAINT fk_book_id FOREIGN KEY (book_id) REFERENCES Books (book_id)
-ON DELETE CASCADE
-ON UPDATE CASCADE;
