a66ed4bf04ff493abf0338b8fcecde96b10e47ee
Babatunde-Odunlami <Odunlamibabatunde@outlook.com>
2024-10-18 16:20:01 -0700

    corecting DB task 5

---
+++ task_5.sql
@@ -0,0 +1,21 @@
+import mysql.connector
+from mysql.connector import Error
+
+mydb =mysql.connector.connect(host="localhost",user="root",password="Temitope@2024#",auth_plugin = "msql_native_password")
+mycursor =mydb.cursor()
+mycursor.execute("USE alx_book_store;")
+
+
+try:
+    # Create a table named `customers` (if it doesn't exist)
+    mycursor.execute(
+        """
+        INSERT INTO Customers(customer_id, customer_name, email, address)
+	VALUES(1, "Cole Baidoo", cbaidoo@sandtech.com","123 Happiness Ave.")
+        """
+    )
+except mysql.connector.Error as e:
+    print(f"Error populating table")
+finally:
+	mycursor.close()
+"task_2.sql" [readonly] 92L, 2456C                                                                                     87,1          Bot
