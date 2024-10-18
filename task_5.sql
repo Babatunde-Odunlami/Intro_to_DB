3984b0d8f641f290a0a8149ff3ac753935e7a48f
Babatunde-Odunlami <Odunlamibabatunde@outlook.com>
2024-10-18 16:23:30 -0700

    corecting DB task 5

--- task_5.sql
+++ task_5.sql
@@ -11,7 +11,7 @@ try:
     mycursor.execute(
         """
         INSERT INTO Customers(customer_id, customer_name, email, address)
-	VALUES(1, "Cole Baidoo", cbaidoo@sandtech.com","123 Happiness Ave.")
+	VALUES(1, "Cole Baidoo", "cbaidoo@sandtech.com","123 Happiness Ave.")
         """
     )
 except mysql.connector.Error as e:
